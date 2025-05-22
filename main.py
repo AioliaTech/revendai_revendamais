be3a30f2d586d11dc512f1e5d4e4894af1b32ffa
AioliaTech <aioliatechrs@gmail.com>
2025-05-22 13:56:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ def get_data(request: Request):
 
                 for palavra in palavras:
                     score = fuzz.token_set_ratio(texto, palavra)
-                    if score >= 55:
+                    if score >= 43:
                         match = True
                         break  # já encontrou, segue pro próximo carro
 
