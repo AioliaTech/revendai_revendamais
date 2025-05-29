414554bb995613b6b8869ecc7d513cdba1d1ef68
AioliaTech <aioliatechrs@gmail.com>
2025-05-29 12:55:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ def converter_preco(valor_str):
         return None
 
 def filtrar_veiculos(vehicles, filtros, valormax=None):
-    campos_textuais = ["modelo", "titulo", "marca", "cor", "categoria", "cambio", "combustivel"]
+    campos_textuais = ["modelo", "titulo"]
     vehicles_filtrados = vehicles.copy()
 
     for chave, valor in filtros.items():
